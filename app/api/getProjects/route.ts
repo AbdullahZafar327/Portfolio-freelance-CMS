877e305beebefdf7e5a11c800d51e36122dfc9cc
RGB PC GAMER <codingbucketorg@gmail.com>
2024-04-11 07:05:28 +0500

    new change

--- app/api/getProjects/route.ts
+++ app/api/getProjects/route.ts
@@ -7,13 +7,15 @@ import { NextRequest, NextResponse } from "next/server";
 export const GET = async (req: NextRequest) => {
     await ConnectedToDb();
     try {
+        const user = await currentUser();
         const {userId} = auth()
+        console.log(user)
 
-        if (!userId) {
+        if (!user || userId) {
             return new NextResponse("Unauthorized user", { status: 404 });
         }
 
-        const projects = await Project.find({ project_user: userId });
+        const projects = await Project.find({ project_user: user._id });
 
         if (projects.length === 0) {
             return new NextResponse("No projects found for the user", { status: 404 });
