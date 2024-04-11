a287bdcd7a396c8e51eda9706084e5d0b7b80f7b
RGB PC GAMER <codingbucketorg@gmail.com>
2024-04-11 06:43:53 +0500

    user fixing

--- app/(User_Dashboard)/dashboard/projects/[...UserId]/page.tsx
+++ app/(User_Dashboard)/dashboard/projects/[...UserId]/page.tsx
@@ -1,13 +1,14 @@
 import ManageProjects from '@/components/projectCards/ManageProjects'
+import { RedirectToSignIn, currentUser } from '@clerk/nextjs'
 import React from 'react'
 
 interface pageProps {
   UserId:string
 }
-const page = ({params}:{params:pageProps}) => {
-  const {UserId} = params
-  if(!UserId){
-    return null
+const page = async ({params}:{params:pageProps}) => {
+  const user = await currentUser()
+  if(!user){
+    return <RedirectToSignIn redirectUrl={"/sign-in"}/>
   }
   
   return (
