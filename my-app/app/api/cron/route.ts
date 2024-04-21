c2c4e08f2e4e3d52ea3f71e66687d795a27277d8
RGB PC GAMER <codingbucketorg@gmail.com>
2024-04-21 09:29:15 +0500

    cron jobs

--- my-app/app/api/cron/route.ts
+++ my-app/app/api/cron/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export const DELETE = async (req: NextRequest, res: NextResponse) => {
   await ConnectedToDb();
   const nineDaysAgo = new Date();
-  nineDaysAgo.setDate(nineDaysAgo.getDate() - 9);
+  nineDaysAgo.setDate(nineDaysAgo.getDate() - 6);
 
   await Project.deleteMany({
     project_status: status.completed,
